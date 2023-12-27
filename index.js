28d14949a99a1d9876472b5e15636e3e852a6706
luizAFS23 <luizafs.1500@gmail.com>
2023-12-27 16:47:01 -0300

    changes

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ button.addEventListener('click', (e) => {
     var city = document.querySelector('.city');
     var state = document.querySelector('.state');
     const country = document.querySelector('.country');
+    const weather_info = document.querySelector('.weather-info');
+
+    if(weather_info.style.display == 'none'){
+        weather_info.style.display = '';
+    }else{
+        weather_info.style.display = 'none';
+    }
 
     async function getWeatherInfo(){
         const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=d5174f5ba9824e42bf0182214232712&q=${city_name}&days=1&aqi=no&alerts=no`, {mode: 'cors'})
