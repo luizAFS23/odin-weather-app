6f39e69702b6648bd96046112aeb3b0d0c7a86cb
luizAFS23 <luizafs.1500@gmail.com>
2023-12-27 16:08:26 -0300

    changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const city = document.getElementById('city-name');
+const button = document.getElementById('button')
 
 async function getWeatherInfo(){
     const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=d5174f5ba9824e42bf0182214232712&q=${city}&days=1&aqi=no&alerts=no`, {mode: 'cors'})
@@ -6,4 +7,8 @@ async function getWeatherInfo(){
     console.log(weatherData);
 }
 
-getWeatherInfo();
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    getWeatherInfo();
+})
