2e11d33903251c46b4dba637baa4003254ead7f9
luizAFS23 <luizafs.1500@gmail.com>
2023-12-27 16:05:31 -0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const city = document.getElementById('city-name');
 
 async function getWeatherInfo(){
-    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=d5174f5ba9824e42bf0182214232712&q=Belém&days=1&aqi=no&alerts=no', {mode: 'cors'})
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=d5174f5ba9824e42bf0182214232712&q=${city}&days=1&aqi=no&alerts=no`, {mode: 'cors'})
     const weatherData = await response.json();
     console.log(weatherData);
 }
