a827c656f620367bbbf0089e642cd2c98399f859
luizAFS23 <luizafs.1500@gmail.com>
2023-12-27 15:59:20 -0300

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const img = document.querySelector('img');
 
 async function getCats(){
-    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=65Wz4JPv0ciQq7Nn3MJX2zOv6DsldlSN&s=cats', {mode: 'cors'})
+    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=d5174f5ba9824e42bf0182214232712&q=Belém&days=1&aqi=no&alerts=no', {mode: 'cors'})
     const catData = await response.json();
     img.src = catData.data.images.original.url;
 }
