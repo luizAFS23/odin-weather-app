6652a728c8f25f075444f1eec0dc994b63477d81
luizAFS23 <luizafs.1500@gmail.com>
2024-01-06 20:13:14 -0300

    changes

--- sla.js
+++ sla.js
@@ -1,8 +1,31 @@
 class LinkedList{
+    constructor(list){
+        this.list = list;
+    }
+
+    get list(){
+        return this.showList();
+    }
+
+    showList(){
+        return this.list;
+    }
+}
+
+class Node{
+    next = null;
+    value = null;
+
+    constructor(element, next){
+        this.value = element;
+        this.next = next;
+    }
+}
+
+function appendValue(value){
+    const node = new Node(value);
 
 }
 
-const Node = function(value, nextNode){
-    this.value = null;
-    this.nextNode = null;
-}
+let linkedlist = new LinkedList();
+console.log(node.next);
