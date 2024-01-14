679785fa58c8d9b4bee342b71dbcdc19bb79aa16
luizAFS23 <luizafs.1500@gmail.com>
2024-01-14 15:21:06 -0300

    changes

--- sla.js
+++ sla.js
@@ -1,5 +1,34 @@
+let buckets = [];
+
 class HashMap{
-    
+
+    constructor(key, value){
+        this.key = key;
+        this.value = value;
+    }
+
+    hash(value){
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for(let i = 0; i < value.length; i++){
+            hashCode = primeNumber * hashCode + String.charCodeAt(i);
+        }
+        return hashCode;
+    }
+
+    set(key, value){
+        if(this.key != null){
+            value = key;
+            return this.value;
+        }
+        return this.value;
+    }
+
+    get(key){
+        if(key )
+    }
+
 }
 
 
